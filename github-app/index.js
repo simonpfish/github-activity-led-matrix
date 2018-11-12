bfeee550072dde2bad5086b6bdef7813a3d30773
simonpfish <simonpfish@gmail.com>
2018-11-12 11:29:24 -0800

    update app config

--- github-app/index.js
+++ github-app/index.js
@@ -2,9 +2,8 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('*', async context => {
+    console.log(context)
   })
 
   // For more information on building apps:
