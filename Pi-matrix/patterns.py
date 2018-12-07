7769d84d7353f0494eec273c66b1dbf1c31c26c8
simonpfish <simonpfish@gmail.com>
2018-12-07 13:21:59 -0800

    initial pixel queue creation

--- Pi-matrix/patterns.py
+++ Pi-matrix/patterns.py
@@ -1,13 +1,33 @@
 import imageio
 import numpy as numpy
 
+frame_queue = []
+
 frames = []
-for i in range (1,36):
-  frame = imageio.imread('firework_pattern/firework-'+str(i)+'.png.png')
+for i in range(1, 36):
+  frame = imageio.imread('firework_pattern/firework-' + str(i) + '.png.png')
   frames.append(frame)
 print(frames)
 
-pixel_queue = []
+
+def queue_frame(frame, conditional=lambda r, c: True):
+  pixel_queue = []
+  for row in range(len(frame)):
+    for col in range(len(frame[row])):
+      pixel = frame[row][col]
+      r, g, b = [1 if bool(p) else 0 for p in pixel[0:3]]
+      address = row * 32 + col
+      if conditional(row, col):
+        pixel_queue.append((address, r, g, b))
+
+  frame_queue.append(pixel_queue)
 
 
+# First frame
+queue_frame(frames[0])
 
+# Rest of frames
+for i in range(1, len(frames)):
+  queue_frame(
+      frames[i],
+      lambda row, col: any(frames[i][row][col] != frames[i - 1][row][col]))
