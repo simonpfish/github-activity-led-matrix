72328ae5d293121c0cc8b8c54646c51685a29043
Maddie Zug <maddiezug@Maddies-MacBook-Pro-2.local>
2018-12-07 12:31:18 -0800

    read in firework pattern

--- Pi-matrix/patterns.py
+++ Pi-matrix/patterns.py
@@ -1,7 +1,11 @@
-from PIL import Image
+import imageio
 import numpy as numpy
 
-imageio.imread
+frames = []
+for i in range (1,36):
+  frame = imageio.imread('firework_pattern/firework-'+str(i)+'.png.png')
+  frames.append(frame)
+print(frames)
 
 pixel_queue = []
 
