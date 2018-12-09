7c445e284bc82dfe2219688ea325904532eb615d
Maddie Zug <maddiezug@Maddies-MacBook-Pro-2.local>
2018-12-09 14:03:51 -0800

    added padding

--- pi/patterns/format_letters.py
+++ pi/patterns/format_letters.py
@@ -3,37 +3,44 @@ import numpy as numpy
 import string
 
 LETTER_WIDTH = 6
-NUM_LETTERS = 32/6
+NUM_LETTERS = 32//6
 
 letters = list(string.ascii_lowercase)
 
 frame_queue = []
+pixel_queue = []
 
 letter_frames = {}
 for i in range(26):
-  frame = imageio.imread('alphabet/alphabet' + str(i) + '.png')
+  frame = imageio.imread('frames/alphabet/alphabet' + str(i) + '.png')
   letter_frames[letters[i]] = frame
-print(letter_frames)
 
-def queue_username(username, start_address):
-  truncated_username = username[:5]
-  for i in range(5):
-    queue_letter_frame(truncated_username[i], start_address+(i*6))
-
-
-
-
-def queue_letter_frame(letter, top_left_address):
-  frame = letter_frames[letter]
+def create_word_frame(word, start_address):
+  frame_queue = []
   pixel_queue = []
+  for n in range(len(word)-NUM_LETTERS+1):
+    truncated_word = word[n:n+NUM_LETTERS]
+    for i in range(NUM_LETTERS):
+      letter = truncated_word[i]
+      top_left_address = start_address+(i*6)
+      queue_letter_pixels(letter, top_left_address)
+    frame_queue.append(pixel_queue)
+
+
+def queue_letter_pixels(letter, top_left_address):
+  frame = letter_frames[letter]
   for row in range(len(frame)):
     for col in range(len(frame[row])):
       pixel = frame[row][col]
       r, g, b = [1 if bool(p) else 0 for p in pixel[0:3]]
-      address = row * 32 + col
+      address = top_left_address + (row * 32) + col
       pixel_queue.append((address, r, g, b))
-
-  frame_queue.append(pixel_queue)
+  for row in range(0, len(frame)+1):
+    address = top_left_address + (row * 32) + len(frame[0])
+    pixel_queue.append((address, 0, 0, 0))
+  for col in range(0, len(frame[0])+1):
+    address = top_left_address + (len(frame) * 32) + col
+    pixel_queue.append((address, 0, 0, 0))
 
 
 
